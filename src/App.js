85a16ee2e568731948b42988662a3037d892f467
Christine Ooi <christine.ooi@gmail.com>
2018-05-22 16:46:11 -0400

    Modified App.js to include a Text component and use it in the App component.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class Text extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: props.color,
+      value: props.value
+    };
+  }
+  render() {
+    return (
+      <span style={{color:this.state.color}}>
+        {this.state.value}
+      </span>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, <Text color="purple" value ="Christine"/></h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
